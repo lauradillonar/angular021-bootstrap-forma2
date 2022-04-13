bf2f61a812713f6089294ba70d5b8f60502f592f
lauradillonar <laura.dillon.ar@gmail.com>
2022-04-13 11:40:41 -0300

    Verificando instalación de bootstrap y jquery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular021-bootstrap-forma2';
+
+  ngOnInit(): void {
+    $(()=> {
+      console.log('ready!');
+
+      $('#my-button').on('click', () => {
+        alert('Hola desde un evento manejado con Jquery');
+      });
+    });
+  }
 }
